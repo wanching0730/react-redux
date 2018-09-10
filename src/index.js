cd3cb143ecdc09f94bd13cf5b23d1d365fff8d4f
wanching_97 <wanching_97@hotmail.com>
2018-09-11 01:46:07 +0800

    npm install

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+
+function reducer () {
+    return "State";
+}
+
+const store = createStore(reducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
