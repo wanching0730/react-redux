2810950ab02ccb65c74322934b806f1a713af610
wanching_97 <wanching_97@hotmail.com>
2018-09-11 09:41:54 +0800

    added store, action, reducer

--- src/index.js
+++ src/index.js
@@ -5,11 +5,29 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 
-function reducer () {
-    return "State";
+function reducer (state, action) {
+    if(action.type === "changeState") {
+        return action.payload.newState;
+    }
+
+    return 'State';
+    
 }
 
 const store = createStore(reducer);
+
+console.log("State in store: " + store.getState());
+
+const action = {
+    type: "changeState",
+    payload: {
+        newState: "New State"
+    }
+};
+
+store.dispatch(action);
+
+console.log("State in store: " + store.getState());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
